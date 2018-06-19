1d4c75ec33ed37ffc0d5a49ed90c8e35818797eb
amantley <amantley@googlemail.com>
2018-06-18 17:58:13 -0700

    added the rotate app script, need to fix the hand angle calculation

--- scripts/developer/rotateApp.js
+++ scripts/developer/rotateApp.js
@@ -8,9 +8,37 @@ var CHANGE_OF_BASIS_ROTATION = { x: 0, y: 1, z: 0, w: 0 };
 var HEAD_TURN_THRESHOLD = 25.0;
 var LOADING_DELAY = 500;
 var AVERAGING_RATE = 0.03;
+
 var activated = false;
 var documentLoaded = false;
 var headPoseAverageOrientation = { x: 0, y: 0, z: 0, w: 1 };
+var hipToLeftHandAverage = { x: 0, y: 0, z: 0, w: 1 };
+var hipToRightHandAverage = { x: 0, y: 0, z: 0, w: 1 };
+var spine2Rotation = { x: 0, y: 0, z: 0, w: 1 };
+var averageAzimuth = 0.0;
+
+var ANIM_VARS = [
+    // "headType",
+    // "spine2Type",
+    // "hipsType",
+    // "spine2Position",
+    "spine2Rotation"
+    // "hipsPosition",
+    // "hipsRotation"
+];
+
+var handlerId = MyAvatar.addAnimationStateHandler(function (props) {
+
+    var result = {};
+    // result.headType = ikTypes.HmdHead;
+    // result.hipsType = props.hipsType;
+    // result.hipsPosition = props.hipsPosition;
+    // result.hipsRotation = props.hipsRotation;
+    // result.spine2Type = props.hipsType;
+    result.spine2Position = spine2Rotation;
+    // result.spine2Rotation = props.hipsRotation;
+    return result;
+}, ANIM_VARS);
 
 // define state readings constructor
 function StateReading(headPose, rhandPose, lhandPose, diffFromAverageEulers) {
@@ -22,7 +50,8 @@ function StateReading(headPose, rhandPose, lhandPose, diffFromAverageEulers) {
 
 // define current state readings object for holding tracker readings and current differences from averages
 var currentStateReadings = new StateReading(Controller.getPoseValue(Controller.Standard.Head),
-    Controller.getPoseValue(Controller.Standard.RightHand), Controller.getPoseValue(Controller.Standard.LeftHand),
+    Controller.getPoseValue(Controller.Standard.RightHand), 
+    Controller.getPoseValue(Controller.Standard.LeftHand),
     { x: 0, y: 0, z: 0 });
     
 // declare the checkbox constructor
@@ -70,7 +99,7 @@ function onKeyPress(event) {
         // when the sensors are reset, then reset the mode.
     }
 }
-
+/*
 function onWebEventReceived(msg) {
     var message = JSON.parse(msg);
     print(" we have a message from html dialog " + message.type);
@@ -114,6 +143,7 @@ function initAppForm() {
  
 }
 
+
 function onScreenChanged(type, url) {     
     print("Screen changed");
     if (type === "Web" && url === HTML_URL) {
@@ -133,37 +163,60 @@ function onScreenChanged(type, url) {
         activated = false;
     }
 }
-
+*/
 function update(dt) {
-    
+
     //  Update head information
     currentStateReadings.headPose = Controller.getPoseValue(Controller.Standard.Head);
     currentStateReadings.rhandPose = Controller.getPoseValue(Controller.Standard.RightHand);
     currentStateReadings.lhandPose = Controller.getPoseValue(Controller.Standard.LeftHand);
-    
+
     var headPoseRigSpace = Quat.multiply(CHANGE_OF_BASIS_ROTATION, currentStateReadings.headPose.rotation);
     headPoseAverageOrientation = Quat.slerp(headPoseAverageOrientation, headPoseRigSpace, AVERAGING_RATE);
     var headPoseAverageEulers = Quat.safeEulerAngles(headPoseAverageOrientation);
-    
+
     if (Math.abs(headPoseAverageEulers.y) > HEAD_TURN_THRESHOLD) {
         //  Turn feet
         MyAvatar.triggerRotationRecenter();
         headPoseAverageOrientation = { x: 0, y: 0, z: 0, w: 1 };
     }
-    
+
+    // and the hand to hip vector to determine when to change head rotation. 
+    // leftHandOrientation = Quat.multiply(MyAvatar.orientation, currentStateReadings.lhandPose.rotation);
+    // rightHandOrientation = Quat.multiply(MyAvatar.orientation, currentStateReadings.rhandPose.rotation);
+    var leftHandPositionRigSpace = Vec3.multiplyQbyV(Quat.inverse(CHANGE_OF_BASIS_ROTATION), currentStateReadings.lhandPose.translation);
+    var rightHandPositionRigSpace = Vec3.multiplyQbyV(Quat.inverse(CHANGE_OF_BASIS_ROTATION), currentStateReadings.rhandPose.translation);
+
+    //  Update angle from hips to hand, to be used for turning 
+    var hipToLeftHandAngle = Vec3.orientedAngle({ x: 0, y: 0, z: 1 }, { x: leftHandPositionRigSpace.x, y: 0, z: leftHandPositionRigSpace.z }, { x: 0, y: 1, z: 0 });
+    var hipToLeftHand = Quat.lookAtSimple({ x: 0, y: 0, z: 0 }, { x: leftHandPositionRigSpace.x, y: 0, z: leftHandPositionRigSpace.z });
+    var hipToRightHand = Quat.lookAtSimple({ x: 0, y: 0, z: 0 }, { x: rightHandPositionRigSpace.x, y: 0, z: rightHandPositionRigSpace.z });
+    hipToLeftHandAverage = Quat.slerp(hipToLeftHandAverage, hipToLeftHand, AVERAGING_RATE);
+    hipToRightHandAverage = Quat.slerp(hipToRightHandAverage, hipToRightHand, AVERAGING_RATE);
+    var leftRightMidpoint = (Quat.safeEulerAngles(hipToLeftHandAverage).y + Quat.safeEulerAngles(hipToRightHandAverage).y) / 2.0;
+    var FILTER_FACTOR = 0.01;
+    averageAzimuth = leftRightMidpoint * (FILTER_FACTOR) + averageAzimuth * (1 - FILTER_FACTOR);
+    spine2Rotation = Quat.angleAxis(averageAzimuth, { x: 0, y: 1, z: 0 });
+    // print("spine 2 orientation " + spine2Rotation.x + " " + spine2Rotation.y + " " + spine2Rotation.z + " " + spine2Rotation.w);
+    // print("average azimuth " + averageAzimuth);
+    print("hip to left hand angle " + hipToLeftHandAngle);
+    // print("left right midpoint " + leftRightMidpoint);
+    // print("left hand position " + leftHandPositionRigSpace.x + " " + leftHandPositionRigSpace.y + " " + leftHandPositionRigSpace.z);
+    // print("right hand position " + rightHandPositionRigSpace.x + " " + rightHandPositionRigSpace.y + " " + rightHandPositionRigSpace.z);
+    // print("hip to left hand average " + hipToLeftHandAverage.x + " " + hipToLeftHandAverage.y + " " + hipToLeftHandAverage.z + " " + hipToLeftHandAverage.w);
 }
 
 function shutdownTabletApp() {
     tablet.removeButton(tabletButton);
     if (activated) {
-        tablet.webEventReceived.disconnect(onWebEventReceived);
+        // tablet.webEventReceived.disconnect(onWebEventReceived);
         tablet.gotoHomeScreen();
     }
-    tablet.screenChanged.disconnect(onScreenChanged);
+    // tablet.screenChanged.disconnect(onScreenChanged);
 }
 
 tabletButton.clicked.connect(manageClick);
-tablet.screenChanged.connect(onScreenChanged);
+// tablet.screenChanged.connect(onScreenChanged);
 
 Script.update.connect(update);
 
